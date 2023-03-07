f86e535699e551bcdc023f34c6c7c29ef04bc1e1
mayank <mayankgupta9450@gmail.com>
2023-03-07 16:45:03 +0530

    Add method in B class

---
+++ src/main/java/com/example/mayank/B.java
@@ -0,0 +1,13 @@
+package com.example.mayank;
+
+public class B {
+
+	public void getMsg() {
+		
+		System.out.println("data published by mayank");
+	}
+public void getMsg1() {
+		
+		System.out.println("data published by mayank");
+	}
+}
