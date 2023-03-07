89411282ccc1df41ac113034df70e4c4248e69a4
mayank <mayankgupta9450@gmail.com>
2023-03-07 13:41:58 +0530

    First commit

---
+++ src/test/java/com/example/mayank/FirstRepositoryProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mayank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRepositoryProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
